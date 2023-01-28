b07de2d1add35a4f3747b2d5e46eda879034814a
Larisa Petrache <larisa.s.petrache@gmail.com>
2023-01-28 22:29:42 +0200

    Updated routes

--- stackoverflow_vue/src/router/index.js
+++ stackoverflow_vue/src/router/index.js
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
     component: HomeView
   },
+  {
+    path: '/login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    component: RegisterView
+  },
+  
 ]
 
 const router = createRouter({
