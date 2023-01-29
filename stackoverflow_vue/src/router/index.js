1620653eb8d48ccc8124ca7cbd8789aaa95b0226
Larisa Petrache <larisa.s.petrache@gmail.com>
2023-01-29 02:01:06 +0200

    Combined login and register pages in a pill
    - Added "LoginRegisterView.vue"
    - Now login and register are on the same page in a pill component
    - Changed route in "index.js"

--- stackoverflow_vue/src/router/index.js
+++ stackoverflow_vue/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
+import LoginRegisterView from '../views/LoginRegisterView.vue'
 import ProfileView from '../views/ProfileView.vue'
 
 const routes = [
@@ -11,11 +10,7 @@ const routes = [
   },
   {
     path: '/login',
-    component: LoginView
-  },
-  {
-    path: '/register',
-    component: RegisterView
+    component: LoginRegisterView
   },
   {
     path: '/profile',
