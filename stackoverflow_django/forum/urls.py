096c80d670c8def1e08ced39116620fa69745aad
Larisa Petrache <larisa.s.petrache@gmail.com>
2023-02-07 08:27:10 +0200

    Revert "Revert "Merge branch 'main' into questions-frontend""
    This reverts commit ed39e0cead7dbdde5e7234d5d28958a998c485c0.

--- stackoverflow_django/forum/urls.py
+++ stackoverflow_django/forum/urls.py
@@ -6,15 +6,15 @@ urlpatterns = [
     path('questions/', views.QuestionsList.as_view()),
     path('answers/', views.AnswersList.as_view()),
     path('categories/', views.CategoriesList.as_view()),
+    path('comments/', views.CommentsList.as_view()),
     path('questions/<slug:category_slug>/', views.CategoryDetail.as_view()),
-    path('queue_users/<slug:user_slug>/', views.QueueUserDetail.as_view()),
+    path('queue_users/<id>/', views.QueueUserDetail.as_view()),
     path('search/', views.search),
     path('ask/', views.ask),
     path('answer/', views.answer),
+    path('comment/', views.comment),
     path('profile/', views.profile),
     path('edit_profile/', views.edit_profile),
     path('queue_users/', views.QueueUsersList.as_view()),
     path('questions/<slug:category_slug>/<slug:question_slug>/', views.QuestionDetail.as_view()),
-    #path('users/<slug:user_slug>/', views.UserDetail.as_view()),
-    #path('categories/', views.CategoryDetail.as_view())
 ]
--- stackoverflow_vue/src/views/HomeView.vue
+++ stackoverflow_vue/src/views/HomeView.vue
@@ -1,10 +1,34 @@
 <template>
     <div class="home">
-        <QuestionBox
-            v-for = "question in latestQuestions"
-            v-bind:key="question.id"
-            v-bind:question="question"/>
+        <div class="card text-bg-dark">
+            <img src="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
+                class="card-img" alt="...">
+            <div class="card-img-overlay text-center" style="background:rgba(0,0,0,0.5)">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h1 class="card-title">Queueunderflow</h1>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+
+                <p class="card-text w-50 m-auto">Queueunderflow is a platform made by programmers, for programmers, that
+                    wishes to accomplish knowledge exchange on a scale unbeforeseen. Users can post questions, interact
+                    with each other and reply with their own approach.</p>
+            </div>
+            <p class="card-text" v-for="question in latestQuestions.slice(0, 4)" v-bind:key="question.id" v-bind:question="question">
+                cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos cos
+            </p >
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -16,7 +40,7 @@ export default {
     name: 'HomeView',
     data() {
         return {
-            latestQuestions: {}
+            latestQuestions: []
         }
     },
     components: {
@@ -24,8 +48,14 @@ export default {
     },
     mounted() {
         document.title = 'Home | Queueunderflow'
-
-        console.log(localStorage.getItem(token))
-    }
+        axios
+            .get('http://localhost:8000/api/v1/questions/')
+            .then(response => {
+                this.latestQuestions = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    },
 }
 </script>
